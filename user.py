a20e9260fc28a158fef31aa89c9f591d80ecf652
TS <tahiryandriatefy00@gmail.com>
2026-02-17 12:59:14 +0300

    Create user.py

---
+++ user.py
@@ -0,0 +1,11 @@
+import os
+import system
+import bs4
+import base64
+import asyncio
+
+def load_activation(arch: default=None):
+  if isinstance(x, y):
+    x = base64(asyncio.run(activate_win(arch))
+    y = bs4(asyncio.run(activate_win_())
+    
